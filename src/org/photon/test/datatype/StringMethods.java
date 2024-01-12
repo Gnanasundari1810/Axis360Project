0ff92a44eb3a98481360def6d7a0925c846c978c
Gnanasundari1810 <sateshbkvp@gmail.com>
2024-01-12 16:28:02 +0530

    Modified by tester 02

--- src/org/photon/test/datatype/StringMethods.java
+++ src/org/photon/test/datatype/StringMethods.java
@@ -2,6 +2,17 @@ package org.photon.test.datatype;
 
 public class StringMethods {
 	
+	
+	private void M1Tester02() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void M2Tester02() {
+		// TODO Auto-generated method stub
+
+	}
+	
 	public static void main(String[] args) {
 		
 		String s = "Daily Practice id Must";
